41ed47296772780ccb4c6b7464cf11d752fe990b
geneeuchoi <chyj2108@naver.com>
2025-01-04 23:11:40 +0900

    메일 센더 테스트

---
+++ src/test/java/com/example/demo/user/service/CertificationServiceTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.user.service;
+
+import com.example.demo.mock.FakeMailSender;
+import com.example.demo.user.service.port.MailSender;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CertificationServiceTest {
+
+    @Test
+    public void 이메일과_컨텐츠가_제대로_만들어져서_보내지는지_테스트한다() {
+        // given
+        FakeMailSender fakeMailSender = new FakeMailSender();
+        CertificationService certificationService = new CertificationService(fakeMailSender);
+
+        // when
+        certificationService.send("test@test.com", 1, "aaaaa-aaa-aaaa-aaaaa");
+
+        // then
+        assertThat(fakeMailSender.email).isEqualTo("test@test.com");
+        assertThat(fakeMailSender.title).isEqualTo("Please certify your email address");
+        assertThat(fakeMailSender.content).isEqualTo("Please click the following link to certify your email address: http://localhost:8080/api/users/1/verify?certificationCode=aaaaa-aaa-aaaa-aaaaa");
+    }
+
+}
